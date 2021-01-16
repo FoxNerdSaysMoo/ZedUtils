6090326b8ce866693c6483f7b7d5428d520cb22d
zeb <zceboys@gmail.com>
2021-01-16 14:34:53 -0600

    Add settings.json to gitignore (privacy purposes)

--- utils/settings.py
+++ utils/settings.py
@@ -4,8 +4,11 @@ import sys
 
 class Settings(dict):
     def __init__(self, source):
-        with open(source, "r") as readfile:
-            super().__init__(**json.load(readfile))
+        try:
+            with open(source, "r") as readfile:
+                super().__init__(**json.load(readfile))
+        except IOError:
+            super().__init__(**{})
 
     async def save(self):
         with open("settings.json", 'w') as writefile:
